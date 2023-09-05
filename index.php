f0f9ad9ea3b2b86352e7d2dcfe23c449a7eaef7d
renataanggunartikasari <renataanggun24@gmail.com>
2023-09-05 12:02:37 +0700

    update

--- index.php
+++ index.php
@@ -9,51 +9,170 @@ $db = new dbController();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap | PHP</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
     <!-- NAVBAR -->
-    <nav class="navbar navbar-expand-lg bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">CRUD</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active" aria-current="page" href="#">Kelas</a>
+                    <a class="nav-link" href="#">Jurusan</a>
+                    <a class="nav-link" href="#">Guru</a>
+                    <a class="nav-link" href="#">Siswa</a>
+                </div>
             </div>
         </div>
     </nav>
     <!-- NAVBAR-END -->
 
-    <!-- CARDS -->
-    
-    <!-- CARDS-END -->
+    <!-- KELAS -->
+    <div class="container" id="kelas">
+        <h1 class="text-center text-center mb-5 mt-70">Kelas</h1>
+        <div class="row text-center justify-content-center">
 
-    <!-- SCRIPT BOOTSTRAP -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+            <?php
+            $sql = "select * from t_kelas";
+            $row = $db->getALL($sql);
+            foreach ($row as $value) :
+            ?>
+
+                <div class="card-all col-md-4 col-xl-4 mb-5">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/kelas/<?php echo $value['f_nama']; ?>.jpg" class="card-img-top" alt="Kelas">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark">
+
+                                <?php echo $value['f_nama']; ?>
+
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+
+            <?php
+            endforeach
+            ?>
+        </div>
+    </div>
+    <!-- KELAS-END -->
+
+    <!-- JURUSAN -->
+    <div class="container" id="jurusan">
+        <h1 class="text-center text-center mb-5 mt-3">Jurusan</h1>
+        <div class="row text-center justify-content-center">
+
+            <?php
+            $sql = "select * from t_jurusan";
+            $row = $db->getALL($sql);
+            foreach ($row as $value) :
+            ?>
+
+                <div class="card-all col-md-4 col-xl-4 mb-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/jurusan/<?php echo $value['f_nama']; ?>.jpg" class="card-img-top" alt="Jurusan">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark">
+                                <?php echo $value['f_nama']; ?>
+                            </h5>
+                            <p class="card-text" style="font-size: 14px;">
+                                <?php echo $value['f_deskripsi']; ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            endforeach
+            ?>
+        </div>
+        <!-- JURUSAN-END -->
+
+        <!-- GURU -->
+        <div class="container" id="guru">
+            <h1 class="text-center text-center mb-5 mt-3">Guru</h1>
+            <div class="row text-center justify-content-center">
+
+                <?php
+                $sql = "select * from t_guru";
+                $row = $db->getALL($sql);
+                foreach ($row as $value) :
+                ?>
+
+                    <div class="card-all col-md-4 col-xl-6 mb-5">
+                        <div class="card" style="width: 18rem;">
+                            <img src="img/guru/<?php echo $value['f_nama']; ?>.jpg" class="card-img-top" alt="Guru">
+                            <div class="card-body">
+                                <h5 class="card-title text-dark">
+
+                                    <?php echo $value['f_nama']; ?>
+
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                endforeach
+                ?>
+            </div>
+            <!-- GURU-END -->
+
+            <!-- SISWA -->
+            <section id="siswa">
+                <div class="row text-center mb-3">
+                    <div class="col">
+                        <h2>Siswa</h2>
+                    </div>
+                </div>
+
+                <div class="row justify-content-center mb-5">
+                    <div class="col-8">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Kelas</th>
+                                    <th scope="col">Jurusan</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+
+                                <?php
+                                $i = 1;
+                                $sql = "select f_nama, f_idkelas, f_idjurusan from t_siswa";
+                                $row = $db->getALL($sql);
+                                foreach ($row as $siswa) :
+
+                                ?>
+
+                                    <tr>
+                                        <th scope="row">
+                                            <?php echo $i++; ?>
+                                        </th>
+                                        <td><?php echo $siswa['f_nama'] ?></td>
+                                        <td><?php echo $siswa['f_idkelas'] ?></td>
+                                        <td><?php echo $siswa['f_idjurusan'] ?></td>
+                                    </tr>
+
+                                <?php endforeach ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+            </section>
+            <!-- SISWA-END -->
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 
 </html>
-
-<?php
-    $sql = "select * from t_kelas";
-    $row = $db->getITEMS($sql);
-    echo $row['f_nama'];
-    ?>
